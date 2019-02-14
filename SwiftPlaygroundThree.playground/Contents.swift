b45c9dc6a92b93d17e15a34f82bc5a7c3bf81e0c
Ryan Muranaka <Rmuranak@gmail.com>
2019-02-14 10:31:11 -1000

    Completed SwiftPlaygroundThree: Introduction to Arrays

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -1,3 +1,29 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//Part 2
+//declaration and assign fruitNames with 3 of your favorite fruits
+var fruitNames = ["Apples", "Bananas", "Watermelons"]
+
+//Part 4
+print(fruitNames)
+
+//Part 5
+print("I like to eat\(fruitNames)")
+//Print out I like to eat ["Apples", "Bananas", "Watermelons"] not what we want
+//String interpolation, this is not an iteration
+
+//Part 6
+for name in fruitNames {
+    print("I like to eat " + name)
+}
+
+//Part 7
+//Print "name" at index 1 in fruitNames
+print(fruitNames[1])
+//Print "name" at index 0 in fruitNames
+print(fruitNames[0])
+
+
+//Assignment #4 STARTS HERE!!
