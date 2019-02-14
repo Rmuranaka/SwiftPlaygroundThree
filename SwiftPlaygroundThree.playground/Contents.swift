bcc73ac6d27d86fafb4d12b5a5843166d163237c
Ryan Muranaka <Rmuranak@gmail.com>
2019-02-14 11:29:14 -1000

    Completed Swift Playground 3 Array Part 2: Array Instance Methods

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -27,3 +27,35 @@ print(fruitNames[0])
 
 
 //Assignment #4 STARTS HERE!!
+//Part 8, Start of Assignment #4
+
+//Part 1: Repeat Parts 2-6 with an array type int
+//Part 2
+//declaration and assign favNumbers with 5 different numbers
+var favNumbers = [1, 2, 3, 4, 5]
+
+//Part 3
+//Print all the number in the favNumbers Array
+print(favNumbers)
+
+//Part 4
+//Print out My favorite number is [1, 2, 3, 4, 5]
+print("My favorite number is" , (favNumbers))
+
+//Part 5
+//Print out My favorite number .............. in 5 lines
+for number in favNumbers {
+    print ("My favorite number is " ,(number))
+}
+
+//Part6
+//Print number  at index 1 in favNumber
+print(favNumbers[1])
+
+//Print number at index 0 in favNumber
+print(favNumbers[0])
+
+//Part 9 Let's Use Array Instance Method To Have Our Arrays Perform New Tasks!
+//Remove indext 2 in numbers array and then print your array
+favNumbers.remove(at: 2)
+print(favNumbers)
